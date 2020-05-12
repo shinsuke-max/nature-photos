25c786c6b994454d6c2449131c085e046a7638bf
shinsuke <shinst710@gmail.com>
2020-05-12 23:59:00 +0900

    post view

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,27 @@
+class PostsController < ApplicationController
+  before_action :authenticate_user!
+  def index
+    @posts = Post.limit(10).order('created_at DESC')
+  end
+  
+  def new
+    @post = current_user.posts.build
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to root_path
+      flash[:notice] = "投稿が保存されました"
+    else
+      redirect_to root_path
+      flash[:alert] = "投稿に失敗しました"
+    end
+  end
+
+  private
+
+    def post_params
+      params.require(:post).permit(:content)
+    end
+end
