1f5193a17bd5a8e8f4c832ca0991671ea0782a30
shinsuke <shinst710@gmail.com>
2020-05-14 04:54:48 +0900

    image-validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,11 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
+  validates :image,   content_type: { in: %w[image/jpeg image/gif image/png],
+                                      message: "この拡張子は無効です" },
+                      size:         { less_than: 5.megabytes,
+                                      message: "サイズが大きいです" }
 end
