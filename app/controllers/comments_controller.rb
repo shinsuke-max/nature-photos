36a75742e0f25b0376e09596acd9ab501f35ca3d
shinsuke <shinst710@gmail.com>
2020-05-15 22:25:59 +0900

    comment-controller and layout comment

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,30 @@
+class CommentsController < ApplicationController
+  before_action :authenticate_user!
+  def create
+    @comment = Comment.new(comment_params)
+    @post = @comment.post
+    if @comment.save
+      redirect_to @post
+      flash[:notice] = "コメントしました"
+    else
+      flash[:alert] = "コメントに失敗しました"
+    end
+  end
+
+  def destroy
+    @comment = Comment.find_by(id: params[:id])
+    @post = @comment.post
+    if @comment.user == current_user
+      @comment.destroy
+      flash[:notice] = "コメントが削除されました"
+    else
+      flash[:alert] = "コメントの削除に失敗しました"
+    end
+    redirect_to root_path
+  end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:user_id, :post_id, :comment)
+    end
+end
