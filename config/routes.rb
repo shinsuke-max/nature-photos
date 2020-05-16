4789dbbfb3044d1c2094e2b7bc1b730dd02cabd3
shinsuke <shinst710@gmail.com>
2020-05-16 23:24:53 +0900

    implement like

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   get '/users/:id', to: 'users#show', as: 'user'
   resources :posts do
     resources :comments, only: %i(create destroy)
+    resources :likes, only: %i(create destroy)
   end
 end
