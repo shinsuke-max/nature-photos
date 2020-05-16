4a68ddf68ca30af481c0b3d214087b2dbcf47a9d
shinsuke <shinst710@gmail.com>
2020-05-17 05:29:47 +0900

    implement 簡単ログイン

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,12 +1,20 @@
 class RegistrationsController < Devise::RegistrationsController
+  before_action :forbid_test_user, {only: [:edit,:update,:destroy]}
 
   protected
 
-  def update_resource(resource, params)
-    resource.update_without_current_password(params)
-  end
+    def update_resource(resource, params)
+      resource.update_without_current_password(params)
+    end
 
-  def after_update_path_for(resource)
-    user_path(resource)
-  end
+    def after_update_path_for(resource)
+      user_path(resource)
+    end
+
+    def forbid_test_user
+      if @user.email == "test@example.com"
+        flash[:notice] = "テストユーザーのため変更できません"
+        redirect_to root_path
+      end
+    end
 end
