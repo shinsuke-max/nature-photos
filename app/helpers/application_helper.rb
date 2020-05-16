4dd98cd88ea5e4b63e7214c21887fcb101ea5bab
shinsuke <shinst710@gmail.com>
2020-05-17 03:31:31 +0900

    add title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,13 @@ module ApplicationHelper
     gravatar_id = Digest::MD5::hexdigest(user.email).downcase
     "https://www.gravatar.com/avatar/#{gravatar_id}.jpg"
   end
+
+  def full_title(page_title)
+    base_title = "Nature-Picture"
+    if page_title.blank?
+      base_title
+    else
+      "#{page_title} - #{base_title}"
+    end
+  end
 end
