74f06859b696c11cc18ba380ad46f13ee24df8d5
shinsuke <shinst710@gmail.com>
2020-05-17 05:30:24 +0900

    add seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,34 @@
 User.create!(name:  "Example User",
-    email: "example@railstutorial.org",
+    email: "example@sample.org",
     password:              "foobar",
     password_confirmation: "foobar")
 
-99.times do |n|
-name  = Faker::Name.name
-email = "example-#{n+1}@railstutorial.org"
-password = "password"
-User.create!(name:  name,
-     email: email,
-     password:              password,
-     password_confirmation: password)
+User.create!(name: "テストユーザー",
+    email: "test@example.com",
+    password: "testtest",
+    password_confirmation: "testtest")
+
+#post = Post.new(content: "きれいなけしき！",
+#user_id: "2")
+#post.image.attach(io: File.open(Rails.root.join('app/assets/images/pic1.jpg')), filename: 'pic1.jpg')
+#post.save
+
+12.times do |n|
+  name  = Faker::Games::Pokemon.name
+  email = "example-#{n+1}@sample.org"
+  password = "password"
+  User.create!(name:  name,
+      email: email,
+      password:              password,
+      password_confirmation: password)
+end
+
+11.times do |p|
+  content = Faker::Alphanumeric.alphanumeric(number: 10)
+  post = Post.new(
+    content: content,
+    user_id: "#{p+1}",
+  )
+  post.image.attach(io: File.open(Rails.root.join("app/assets/images/seed/image-#{p+1}.jpg")), filename: "image-#{p+1}.jpg")
+  post.save
 end
